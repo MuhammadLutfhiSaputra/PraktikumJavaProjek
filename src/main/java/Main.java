b87b828825b3faaecdf084e30db48c8c774c60f3
camelion <mikroandi211@gmail.com>
2022-07-01 14:17:31 +0800

    KecamatanInputFrame dana KecamatanInputFrame meniru dari kabupaten

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import helpres.Koneksi;
 import frame.KabupatenViewFrame;
+import frame.KecamatanViewFrame;
 
 
 public class Main {
     public static void main(String[] args) {
 //        Koneksi.getConnection();
-        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+//        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+        KecamatanViewFrame viewFrame = new KecamatanViewFrame();
         viewFrame.setVisible(true);
 
     }
